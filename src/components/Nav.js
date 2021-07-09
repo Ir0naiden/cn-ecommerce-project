0e3be8ea98ed66d9d1cd1cbf5d5494bb23f563b7
Aiden Baldwin <aidenbaldwin1985@gmail.com>
2021-07-09 14:12:36 +0100

    updated prior to moving data onclick to display in modal

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Modal } from "react-responsive-modal";
 
-class App extends React.Component {
+class Nav extends React.Component {
   state = {
-    open: false
+    open: false,
   };
 
   onOpenModal = () => {
@@ -18,16 +18,16 @@ class App extends React.Component {
     const { open } = this.state;
     return (
       <div>
-        <button className="cart" onClick={this.onOpenModal}>View cart</button>
+        <button className="cart" onClick={this.onOpenModal}>
+          View cart
+        </button>
         <Modal open={open} onClose={this.onCloseModal}>
           <h2>Your shopping cart</h2>
-          <p>
-            Buy these f**king cats, we're sick of the sight of them.
-          </p>
+          <p>Buy these f**king cats, we're sick of the sight of them.</p>
         </Modal>
       </div>
     );
   }
 }
 
-export default App;
+export default Nav;
